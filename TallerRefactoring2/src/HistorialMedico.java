d2fcbc641ade0513f7a050a010e888138d18e3fd
Xavih830 <120606644+Xavih830@users.noreply.github.com>
2024-08-07 21:14:37 -0500

    Update HistorialMedico.java

--- TallerRefactoring2/src/HistorialMedico.java
+++ TallerRefactoring2/src/HistorialMedico.java
@@ -18,11 +18,35 @@ public class HistorialMedico {
         this.consultas = consultas;
     }
 
+    public void addConsulta(Consulta consulta) {
+        consultas.add(consulta);
+    }
+
+    public void deleteConsulta(Consulta consulta) ElementNotFoundException {
+        int index = consultas.indexOf(consulta)
+        if (index == -1) {
+            throw new ElementNotFoundException("Consulta no encontrada: " + consulta);
+        } 
+        consultas.remove(index);
+    }
+    
     public List<RecetaMedica> getRecetasMedicas() {
         return recetasMedicas;
     }
 
     public void setRecetasMedicas(List<RecetaMedica> recetasMedicas) {
         this.recetasMedicas = recetasMedicas;
+    }
+
+    public void addReceta(RecetaMedica receta) {
+        recetasMedicas.add(receta);
+    }
+
+    public void deleteReceta(RecetaMedica receta) {
+        int index = recetasMedicas.indexOf(receta)
+        if (index == -1) {
+            throw new ElementNotFoundException("Receta no encontrada: " + receta);
+        }
+        recetasMedicas.remove(index);
     }
 }
