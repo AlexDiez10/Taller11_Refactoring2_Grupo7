89dbfa63abef4150e7621e7e73d6b1b3dae7efa7
Xavih830 <120606644+Xavih830@users.noreply.github.com>
2024-08-07 21:49:20 -0500

    Update Paciente.java

--- TallerRefactoring2/src/Paciente.java
+++ TallerRefactoring2/src/Paciente.java
@@ -1,10 +1,18 @@
 public class Paciente extends Persona {
-    public HistorialMedico historialMedico;
+    private HistorialMedico historialMedico;
     public Paciente(String nombre, int edad, String genero, String direccion, String telefono, String correoElectronico) {
         super(nombre, edad, genero, direccion, telefono, correoElectronico);
         this.historialMedico = new HistorialMedico();
     }
 
+    public HistorialMedico getHistorialMedico(){
+        return this.historialMedico;
+    }
+
+    public void setHistorialMedico(HistorialMedico historialMedico){
+        this.historialMedico = historialMedico;
+    }
+    
     public boolean esAdultoMayor(){
         return super.getEdad()>=65;
     }
